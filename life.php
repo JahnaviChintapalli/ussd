30779ee09903ff71597ec1474ee90dbd6c689700
SathvikaBrahmandlapally <136707864+SathvikaBrahmandlapally@users.noreply.github.com>
2023-06-15 17:10:37 +0530

    Life

---
+++ life.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+$sessionId = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text = $_POST["text"];
+
+$exp=explode('*',$text);
+$store = array(); 
+
+if($text == ""){
+    $response = "CON Please select your Insurance type \n";
+    $response .= "1. Health \n";
+    $response .= "2. Motor \n";
+    $response .= "3. Life";
+}
+
+else if($text == "3"){
+    $response = "CON Please enter your Name \n";
+    $store["Insurance"] = "Life" ;
+
+}
+
+else if($exp[0]=="3" && $exp[1] && !$exp[2]){
+    $store["Name"] = $exp[1];
+    $response = "CON Please Enter your Date of Birth (ex:12-09-2001) \n";
+}
+else if($exp[0] == "3" &&($exp[2]) && (!$exp[3])){
+    $response = "CON Enter your Phone Number\n" ;
+    $store["DOB"] = $exp[2] ;
+}
+else if ($exp[0] == "3" &&($exp[3]) && (!$exp[4])){
+	$store["Phone Number"] = $exp[3];
+    $response = "CON Do you Smoke? \n " ;
+    $response .= "1.Yes \n";
+    $response .= "2.No \n";
+}
+else if($exp[0] == "3" &&($exp[4] == "1") && (!$exp[5])){
+    $response = "CON Do you have any Disease? \n";
+    $response .= "1.Yes \n";
+    $response .= "2.No \n";
+    $store["isSmoker"] = "Yes" ;
+}
+else if($exp[0] == "3" &&($exp[4] == "2") && (!$exp[5])){
+    $response = "CON Do you have any Disease? \n";
+    $response .= "1.Yes \n";
+    $response .= "2.No \n";
+    $store["isSmoker"] = "No" ;
+}
+ else if ($exp[0] == "3" && ($exp[5] == "1") && (!$exp[6])){
+    $response = "CON Please enter your Occupation \n";
+    $store["PED"] = "Yes" ;
+} 
+ else if ($exp[0] == "3" && ($exp[5] == "2") && (!$exp[6])){
+    $response = "CON Please enter your Occupation \n";
+    $store["PED"] = "No" ;
+}
+else if ($exp[0] == "3" && ($exp[6]) && (!$exp[7])){
+    $response = "CON Please enter your Salary(eg. 1000000) \n";
+    $store["Ocuupation"] = $exp[6];
+}
+else if ($exp[0] == "3" && (($exp[7])) && (!$exp[8])){
+	$store["Salary"] = $exp[7];
+    if ((int)$exp[5] > 0) {
+        $response = "CON Please enter your Education \n";
+    } else {
+        $response = "END Sorry you entered Invalid Salary \n";
+    } 
+}
+else if ($exp[0] == "3" && ($exp[8]) && (!$exp[9])){
+    $response = "CON Please Select the Sum Insured \n";
+    $response .= "1.25,00,000 \n";
+    $response .= "2.50,00,000 \n";
+    $response .= "3.1,00,00,000 \n";
+    $response .= "3.2,00,00,000 \n";
+    $store["Education"] = $exp[8];
+}
+else if ($exp[0] == "3" && $exp[9] == "1" && (!$exp[10])) {
+	$response = "CON Top 5 quotes for You \n";
+    $store["SumInsured"] = "2500000" ;
+}
+else if ($exp[0] == "3" && $exp[9] == "2" && (!$exp[10])) {
+	$response = "CON Top 5 quotes for You \n";
+    $store["SumInsured"] = "5000000" ;
+}
+else if ($exp[0] == "3" && $exp[9] == "3" && (!$exp[10])) {
+	$response = "CON Top 5 quotes for You \n";
+    $store["SumInsured"] = "10000000" ;
+}
+else if ($exp[0] == "3" && $exp[9] == "4" && (!$exp[10])) {
+	$response = "CON Top 5 quotes for You \n";
+    $store["SumInsured"] = "20000000" ;
+}
+else if ($exp[0]=="3" && ($exp[10])&& (!$exp[11])){
+    $response = "CON Confirmation for policy booking \n" ;
+    $response .= "1.Yes \n" ;
+    $response .= "2.No \n";
+   
+} else if($exp)
+{
+    $response = "END Thank you for your response";
+}
+header('Content-type:text/plain');
+echo  $response;
+
+?>
+
+</body>
+</html>
